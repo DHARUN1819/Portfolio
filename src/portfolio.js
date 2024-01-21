d59f8a69515493a28b2ef826c637b6d81636a9db
Dharun <dharundeepak1819@gmail.com>
2024-01-21 22:21:08 +0530

    commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://dharun1819.github.io/Portfolio',
   title: 'JS.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Dharun P',
+  role: 'Full Stack Developer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'Welcome to my web development portfolio! I am Dharun, a passionate and experienced Full Stack Developer dedicated to crafting interactive, user-friendly, and visually appealing websites. With a strong foundation in coding and a keen eye for design, I bring creativity and technical expertise to every project.',
+  resume: 'https://dharun-resume.tiiny.site/',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/dharun-periyasamy/',
+    github: 'https://github.com/DHARUN1819/',
   },
 }
 
@@ -21,27 +21,27 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Hostel Management System',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+      'Hostel management system is a web application designed to streamline the management and administration of hostels .The application can be accessed by both the hostel staff and the students through a web-based interface.',
+    stack: ['Django', 'Python', 'SQLite'],
+    sourceCode: 'https://github.com/DHARUN1819/Hostel-Management-System-Django',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'Airline Reservation System',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+      'Airline reservation system is a web application that is used to manage and automate the booking process for flights. It allows customers to create an account ,search for flights and make reservations',
+    stack: ['PHP', 'MySql',],
+    sourceCode: 'https://github.com/DHARUN1819/Airline-Reservation-System',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 3',
+    name: 'MERN Stack Signup/Login Page',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+      'Sample Signup/Login website built using MERN stack',
+    stack: ['MERN', 'Javascript',],
+    sourceCode: 'https://github.com/DHARUN1819/MERN_Stack_Signup_Application',
     livePreview: 'https://github.com',
   },
 ]
@@ -49,23 +49,20 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
+  'Java',
   'HTML',
   'CSS',
   'JavaScript',
-  'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'MongoDB',
+  'Node',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'dharundeepak1819@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
